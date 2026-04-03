f5c223737dda73cb02a3fe82244602421712badd
shaharetikesh-cloud <shaharetikesh@gmail.com>
2026-04-03 09:09:08 +0530

    Refactor access_flags to admin_badge_context

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,20 +1,21 @@
-from .permissions import ADMIN_ROLES, APPROVER_ROLES, get_allowed_substations, get_user_role
+from .models import SignupRequest
+from .permissions import ADMIN_ROLES, get_user_role
 
 
-def access_flags(request):
-    user = getattr(request, 'user', None)
-    if not user or not user.is_authenticated:
+def admin_badge_context(request):
+    if not request.user.is_authenticated:
         return {
-            'nav_role': None,
-            'nav_is_admin': False,
-            'nav_is_approver': False,
-            'nav_allowed_substation_count': 0,
+            'pending_signup_count': 0,
         }
 
-    role = get_user_role(user)
+    role = get_user_role(request.user)
+    if role in ADMIN_ROLES:
+        return {
+            'pending_signup_count': SignupRequest.objects.filter(
+                status=SignupRequest.STATUS_PENDING
+            ).count()
+        }
+
     return {
-        'nav_role': role,
-        'nav_is_admin': role in ADMIN_ROLES,
-        'nav_is_approver': role in APPROVER_ROLES,
-        'nav_allowed_substation_count': get_allowed_substations(user).count(),
+        'pending_signup_count': 0,
     }
