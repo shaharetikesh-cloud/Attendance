75848cc5440cb10fbd459db2e42ebc9e13ce9c0d
shaharetikesh-cloud <shaharetikesh@gmail.com>
2026-04-03 08:52:01 +0530

    Delete static directory

--- static/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll("input, select, textarea").forEach((element) => {
-        if (!element.classList.contains("form-check-input")) {
-            element.classList.add(element.tagName === "SELECT" ? "form-select" : "form-control");
-        }
-    });
-});
