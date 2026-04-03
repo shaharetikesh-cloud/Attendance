d559a26a826f0a168ed9f6f85d1dad8fc9239881
shaharetikesh-cloud <shaharetikesh@gmail.com>
2026-04-03 09:12:48 +0530

    Replace access_flags context processor with admin_badge_context

--- msedcl_easy_attendance/settings.py
+++ msedcl_easy_attendance/settings.py
@@ -44,7 +44,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'core.context_processors.access_flags',
+                'core.context_processors.admin_badge_context',
             ],
         },
     },
