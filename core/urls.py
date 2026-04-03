5af7ea323c7d72a9cae008deb2cae8b46037852e
shaharetikesh-cloud <shaharetikesh@gmail.com>
2026-04-03 07:25:20 +0530

    Refactor login path to improve code formatting
    Refactor login path for better readability.

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,14 @@ from .views import dashboard, help_manual, signup_requests, signup_view, system_
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', redirect_authenticated_user=True), name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='registration/login.html',
+            redirect_authenticated_user=True
+        ),
+        name='login'
+    ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', signup_view, name='signup'),
     path('user-management/', user_management, name='user_management'),
